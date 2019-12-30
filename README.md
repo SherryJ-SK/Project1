c244cdbdf01061952ea97edde93cc774f5ab7d78
Vegeta <56867717+pozengineer@users.noreply.github.com>
2019-12-30 21:55:18 +1100

    cards created successfully from ajax search and success/error validation implemented

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # Project1
+
+Summary
+
+The web application is an Event search engine that enables the user to
+
+The application uses 'addEventListener' so that when one of the buttons is clicked on, it will initiate
+the associated function. The use of 'preventDefault' and 'stopPropagation' methods are used to prevent
+the window from going to it's default settings when the function is run and prevents the clicked event
+from affecting the parent elements in the application. Even though the application illustrates no noticable
+bugs, there are a few things that were not rectified due to time restrictions. With additional time to work
+on the code, this issue could be fixed. 
+
+The purpose of this application is to demonstrate the use of functions and 'addEventListener' so that events
+are able to be delegated to different elements in an html file. This application demonstrates how javaScript
+can be utilised to create and append elements to the HTML document. This allows manipulation of element
+attributes during certain times when the application is being used. In addition, implementing responsive
+behaviour of html files using media queries. This enables the developer to create applications that can be
+viewed on multiple devices and screen sizes such as:
+- iphone
+- Tablet
+- Laptop
+- Desktop
+
+The following methods, objects and API's (Application programming Interface) were used in this web
+application to demonstrate how javaScript can be used to apply calculations and functions to elements
+in an html file:
+- Arrays 
+- Functions
+- IF Statements
+- addEventListener
+- preventDefault method
+- stopPropagation method
+- Local Storage
+- Create Elements
+- Set Attributes
+- AppendChild
+- Moment.js
+- Interval
+
+Getting Started
+
+This documentation will assist you in viewing this project. To view the project either open
+the url of the deployed application with the following address in any browser:
+
+https://pozengineer.github.io/weatherDashboard/
+
+Download and clone the repository from GitHub using the following command:
+
+git clone https://github.com/pozengineer/weatherDashboard.git
+
+The following files are used to create the application:
+- index.html
+- reset.css
+- style01.css
+- javaScript01.js
+- moment.min.js
+There are additional files that should be disregarded. They were used as the initial building
+blocks for the application but want to keep them for future reference.
+
+This application was built using:
+-   HTML: HyperText Markup Language that allows the developer to structure webpages
+-   CSS: Style Sheet Language that allows the developer to style an HTML document
+-   JavaScript: Programming Language that allows the developer to apply functions and methods to an HTML document 
+-   Materialize: Library of HTML and CSS files and code https://materializecss.com/
+-   jQuery: JavaScript library that enables HTML document traversal and manipulation, event handling, animation,
+    and Ajax much simpler with an easy-to-use API
+
+![myEvents desktop](assets/images/myEvents01.jpg)
+![myEvents tablet](assets/images/myEvents02.jpg)
+![myEvents iphoneX](assets/images/myEvents03.jpg)
--- assets/javascript/index-page-js.js
+++ assets/javascript/index-page-js.js
@@ -2,23 +2,24 @@ $(document).ready(function() {
 
     var currentTime = $("#currentTime");
     currentTime.text(moment().format('lll'));
-
-    // var apiKey = "prBtwnYW6qBfgHYfb8kiUfLXT4bAjXap" //TicketMaster
-    var apiKey = "93sIViJs6exJuIwcNtKqMSGIAdp1eJV9" //backup
+    // TicketMaster
+    // var apiKey = "prBtwnYW6qBfgHYfb8kiUfLXT4bAjXap"
+    // backup
+    var apiKey = "93sIViJs6exJuIwcNtKqMSGIAdp1eJV9" 
     var checker = false;
     var deviceLat = "";
     var deviceLon = "";
 
-    //search button start parse from API on click
+    // search button start parse from API on click
     $("#searchBtn").on("click", function() {
         event.preventDefault();
         $(".displayEvents").empty();
-        localStorage.removeItem("events"); //clean localstorage
+        localStorage.removeItem("events"); // clean localstorage
         grabResponse();
     });
-    //get current device location
+    // get current device location
     getDeviceLoc();
-    //get current device location function
+    // get current device location function
     function getDeviceLoc() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(geoSucc, showError);
@@ -35,45 +36,45 @@ $(document).ready(function() {
                     // "User denied the request for Geolocation."
                     break;
                 case error.POSITION_UNAVAILABLE:
-                    //  "Location information is unavailable."
+                    // "Location information is unavailable."
                     break;
                 case error.TIMEOUT:
-                    //  "The request to get user location timed out."
+                    // "The request to get user location timed out."
                     break;
                 case error.UNKNOWN_ERROR:
-                    //  "An unknown error occurred."
+                    // "An unknown error occurred."
                     break;
             };
         }
     }
 
-    //parse search result from API
+    // parse search result from API
     function grabResponse() {
-        //keyword inputed by user
+        // keyword inputed by user
         var searchInput = $("#searchInput").val();
 
-        //country set as AU changed by using geolocation
-        //var country = "&countryCode=AU";
+        // country set as AU changed by using geolocation
+        // var country = "&countryCode=AU";
 
-        //Classification, set as default none if no input from user
+        // Classification, set as default none if no input from user
         if ($("#category").val() == null) {
             var classification = "";
         } else {
             classification = "&classificationName=" + $("#category").val();
         };
-        //responsesize, set as default 5 if user input is null
+        // responsesize, set as default 5 if user input is null
         if ($("#numberSelector").val() == "") {
             var responseNumber = "&size=5"; //response size
         } else {
             responseNumber = "&size=" + $("#numberSelector").val();
         };
-        //reponse of the location of current device
+        // reponse of the location of current device
         if (deviceLat == "" && deviceLon == "") {
             var geoSearch = ""; //response size
         } else {
             geoSearch = "&latlong=" + deviceLat + "," + deviceLon;
         };
-        //API from ticketmaster
+        // API from ticketmaster
         console.log(searchInput);
         console.log(classification);
         console.log(responseNumber);
@@ -81,18 +82,29 @@ $(document).ready(function() {
 
         var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchInput + responseNumber + classification + "&apikey=" + apiKey;
         console.log(queryURL);
-        //parse response from ticketmaster and enter displayEvent function
+        // parse response from ticketmaster and enter displayEvent function
         $.ajax({
+            type:'GET',
             url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response);
-            displayEvents(response);
+            dataType:'JSON',
+            success: function(response) {
+                console.log(response);
+                displayEvents(response);
+            },
+            error: function(){
+                alert("Error, 404 (not found)!")
+            }
         });
-
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // }).then(function(response) {
+        //     console.log(response);
+        //     displayEvents(response);
+        // });
     };
 
-    //display more search parameters on click
+    // display more search parameters on click
     $("#arrowDown").click(function() {
         event.preventDefault();
 
@@ -105,94 +117,115 @@ $(document).ready(function() {
         }
     })
 
-    //display search response for home page
+    // display search response for home page
     function displayEvents(response) {
         console.log(response);
+        var arrayLength = eventDisplayNumber();
+        console.log(arrayLength);
         //loop through search response to display them
-        for (var i = 0; i < 20; i++) {
-            //parse from json for the needed data
-            var eventEl = response._embedded.events[i];
-            var eventDate = eventEl.dates.start.localDate;
-            var newDate = moment(eventDate).format("MMMM Do YYYY");
-            var eventTime = eventEl.dates.start.localTime;
-            var newTime = moment(eventTime, "HH:mm:ss").format("h:mm a");
-            var eventImg = eventEl.images[0].url;
-            var eventHeader = eventEl.name;
-            var eventLink = eventEl.url;
-            var eventId = eventEl.id;
-            //display in display area using card 
-            var displayBox = $(".displayEvents");
-            var cardContainer = $("<div class=\"card mt-2 saveEventDiv\" style=\"width: 18rem;\">");
-            cardContainer.append("<img src=" + eventImg + " class=\"card-img-top\" alt=" + eventHeader + "></img>");
-            var cardBody = $("<div class=\"card-body\">");
-            var cardLink = $("<a class=\"links\" href=" + eventLink + ">");
-            cardLink.append("<h5 class=\"card-title\" id=" + eventHeader + ">" + eventHeader + "</h5>");
-            cardBody.append(cardLink);
-            cardBody.append("<p class=\"card-text time\">" + newTime + "</p>");
-            cardBody.append("<p class=\"card-text date\">" + newDate + "</p>");
-            cardBody.append("<button class=\"btn btn-primary saveEvent\" id=" + eventId + "><i class=\"fa fa-star\"></i></button>"); //id saveEvent for like buttons
-
-            cardContainer.append(cardBody);
-            displayBox.append(cardContainer);
-
-            var savedNameString = localStorage.getItem("events");
-            savedNameJSON = JSON.parse(savedNameString) || [];
-
-            var newInput = [{
-                "eventId": eventId,
-                "eventName": eventHeader,
-                "eventDate": eventDate,
-                "eventTime": eventTime,
-                "eventImg": eventImg,
-                "eventLink": eventLink
-            }];
-
-            savedNameJSON.push(newInput);
-            localStorage.setItem("events", JSON.stringify(savedNameJSON));
-        };
-
+        var eventExist = response._embedded;
+        console.log(eventExist);
+        if (eventExist == null) {
+            alert('Error, no response received!')
+        }
+        else {
+            for (var i = 0; i < arrayLength; i++) {
+                // parse from json for the needed data
+                var eventEl = response._embedded.events[i];
+                var eventDate = eventEl.dates.start.localDate;
+                var newDate = moment(eventDate).format("MMMM Do YYYY");
+                var eventTime = eventEl.dates.start.localTime;
+                var newTime = moment(eventTime, "HH:mm:ss").format("h:mm a");
+                var eventImg = eventEl.images[0].url;
+                var eventHeader = eventEl.name;
+                var eventLink = eventEl.url;
+                var eventId = eventEl.id;
+                // display in display area using card 
+                var displayBoxEl = $('.displayEvents');
+                var cardContainerEl = $('<div>').addClass('col s4');
+                var cardDivEl = $('<div>').addClass('card saveEventDiv');
+                var imageDivEl = $('<div>').addClass('card-image');
+                var imageEl = $('<img>');
+                var cardLinkEl = $('<a>').addClass('card-action links card-title');
+                var cardContentEl = $('<div>').addClass('card-content');
+                var eventTimeEl = $('<p>').addClass('card-text time'); 
+                var eventDateEl = $('<p>').addClass('card-text date');
+                var saveEventBtnEl = $('<a>').addClass('waves-effect waves-light btn saveEvent');
+                var saveSymbolEl = $('<i>').addClass('fa fa-star');
+                
+                imageEl.attr('src', eventImg);
+                imageEl.attr('alt',eventHeader);
+                imageEl.css('width', '100%');
+                cardLinkEl.attr('href', eventLink);
+                cardLinkEl.attr('id', eventHeader);
+                cardLinkEl.html('<h5>' + eventHeader + '</h5>');
+                eventTimeEl.html('<p>' + newTime + '</p>');
+                eventDateEl.html('<p>' + newDate + '</p>');
+                saveEventBtnEl.attr('id', eventId);     
+    
+                // var cardContainer = $("<div class=\"card mt-2 saveEventDiv\" style=\"width: 18rem;\"</div>");
+                // cardContainer.append("<img src=" + eventImg + " class=\"card-img-top\" alt=" + eventHeader + "></img>");
+                // var cardBody = $("<div class=\"card-body\">");
+                // var cardLink = $("<a class=\"links\" href=" + eventLink + ">");
+                // cardLink.append("<h5 class=\"card-title\" id=" + eventHeader + ">" + eventHeader + "</h5>");
+                // cardBody.append(cardLink);
+                // cardBody.append("<p class=\"card-text time\">" + newTime + "</p>");
+                // cardBody.append("<p class=\"card-text date\">" + newDate + "</p>");
+                // cardBody.append("<button class=\"btn btn-primary saveEvent\" id=" + eventId + "><i class=\"fa fa-star\"></i></button>");
+                
+                // id saveEvent for like buttons
+                
+                displayBoxEl.append(cardContainerEl);
+                cardContainerEl.append(cardDivEl);
+                cardDivEl.append(imageDivEl);
+                imageDivEl.append(imageEl);
+                cardDivEl.append(cardLinkEl);
+                cardDivEl.append(cardContentEl);
+                cardContentEl.append(eventTimeEl);
+                cardContentEl.append(eventDateEl);
+                cardDivEl.append(saveEventBtnEl);
+                saveEventBtnEl.append(saveSymbolEl);
+    
+                var savedNameString = localStorage.getItem("events");
+                savedNameJSON = JSON.parse(savedNameString) || [];
+    
+                var newInput = [{
+                    "eventId": eventId,
+                    "eventName": eventHeader,
+                    "eventDate": eventDate,
+                    "eventTime": eventTime,
+                    "eventImg": eventImg,
+                    "eventLink": eventLink
+                }];
+    
+                savedNameJSON.push(newInput);
+                localStorage.setItem("events", JSON.stringify(savedNameJSON));
+            };
+        }
     };
+    
+    function eventDisplayNumber() {
+        // if there is no value in the 'number of events displayed' inputBox, then
+        // array size will be 5 elements
+        if ($("#numberSelector").val() == "") {
+            var displayNumber = 5;
+        }
+        // if the above condition is not met, then the array size will be equivalent
+        // to the number input by the user
+        else {
+            displayNumber = $("#numberSelector").val();
+        }
+        return displayNumber;
+    }
 
-    //search results has a "like" button to add them into favourite page
-    //on click these "like" buttons can add them to local storage
-    // $(".displayEvents").on("click", ".saveEvent", saveEventToLocal);
-
-    // function saveEventToLocal() {
-    //     console.log(this);
-
-    //     var savedName = $(this).attr("eventHeader");
-    //     var filted = false;
-    //     var newInput = [{
-    //         "eventName": savedName,
-    //     }];
-
-    //     var events = JSON.parse(localStorage.getItem("events"));
-    //     if (events == null) {
-    //         events = [newInput];
-    //         localStorage.setItem("events", JSON.stringify(events));
-    //     } else {
-    //         //loop through localstorage first to see if there is repetitive events
-    //         //to prevent from repetitive saving
-    //         for (var i = 0; i < events.length; i++) {
-    //             if (savedName === events[i][0].savedName) {
-    //                 filted = true;
-    //             }
-    //         };
-
-    //         if (filted == false) {
-    //             events.push(newInput);
-    //             localStorage.setItem("events", JSON.stringify(events));
-    //         }
-    //     }
-    // }
-
-    //button clicked to save event into favourite page
-    //grab information from existing array and store into new array show in favourite page
+    // button clicked to save event into favourite page
+    // grab information from existing array and store into new array show in favourite page
     $(".displayEvents").on("click", ".saveEvent", saveEventToLocal);
 
     function saveEventToLocal() {
 
-        var savedName = $(this).attr("id");
+        var savedName = $(this).attr('id');
+        console.log(savedName);
         var eventsList = JSON.parse(localStorage.getItem("events"));
 
         // create new array using the information in existing array
@@ -219,9 +252,10 @@ $(document).ready(function() {
 
                 }
             }
+
         } else {
-            //check if event has been stored in the new array
-            //if event alreadd exist, it won't be double saved
+            // check if event has been stored in the new array
+            // if event alreadd exist, it won't be double saved
             var checker = search(savedName, selectedJSON);
             console.log(checker);
             if (checker) {
@@ -243,7 +277,6 @@ $(document).ready(function() {
                         selectedJSON.push(selectedInput);
                         localStorage.setItem("selected", JSON.stringify(selectedJSON));
                     }
-
                 }
             }
         }
@@ -258,3 +291,32 @@ $(document).ready(function() {
         }
     }
 });
+
+// search results has a "like" button to add them into favourite page
+// on click these "like" buttons can add them to local storage
+// $(".displayEvents").on("click", ".saveEvent", saveEventToLocal);
+// function saveEventToLocal() {
+//     console.log(this);
+//     var savedName = $(this).attr("eventHeader");
+//     var filted = false;
+//     var newInput = [{
+//         "eventName": savedName,
+//     }];
+//     var events = JSON.parse(localStorage.getItem("events"));
+//     if (events == null) {
+//         events = [newInput];
+//         localStorage.setItem("events", JSON.stringify(events));
+//     } else {
+//         //loop through localstorage first to see if there is repetitive events
+//         //to prevent from repetitive saving
+//         for (var i = 0; i < events.length; i++) {
+//             if (savedName === events[i][0].savedName) {
+//                 filted = true;
+//             }
+//         };
+//         if (filted == false) {
+//             events.push(newInput);
+//             localStorage.setItem("events", JSON.stringify(events));
+//         }
+//     }
+// }
