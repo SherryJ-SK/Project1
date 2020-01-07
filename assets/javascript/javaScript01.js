7a83ff196fedcfe6f954522ceb7e8af040a0ed1b
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-01-07 22:17:28 +1100

    created advertisement carousel using geoSearch

--- assets/javascript/javaScript01.js
+++ assets/javascript/javaScript01.js
@@ -1,11 +1,5 @@
 
 $(document).ready(function() {
     $('.sidenav').sidenav();
-    $('.carousel').carousel({
-        indicators: true
-    });
-
-    setInterval(function(){
-        $('.carousel').carousel('next');
-    }, 2000);
+    
 });
